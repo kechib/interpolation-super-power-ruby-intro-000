d3fb2eac6394e67ea52b764b025c421e8e24e167
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-21 16:20:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,3 @@
 def display_rainbow(colors)
 puts "R: red #{colors[0]}, O: orange #{colors[1]}, Y: yellow #{colors[2]}, G: green #{colors[3]}, B: blue #{colors[4]}, I: indigo #{colors[5]}, V: violet #{colors[6]} "
 end
-
-
